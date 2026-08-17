324067f4eba3657cbc2649cf514e5363c415096e
Gaurav <gauravkhatri492006@gmail.com>
2026-08-17 09:47:12 +0530

    Initial SwiftUI project setup

---
+++ Mochi/Mochi/MochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MochiApp.swift
+//  Mochi
+//
+//  Created by Gaurav on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
